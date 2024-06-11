a0db4cccae66ccb48794c0cc3b10017c13d94599
JurBri <89058157+JurBri@users.noreply.github.com>
2024-06-11 01:47:05 +0000

    Se crea el semaforo y toda la configuracion para q cambie de color al darl un click a los circulos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("red");
+
+
+	useEffect(() => {
+		console.log(`El color activo es: ${color}`)
+	}, [color])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column align-item-center justify-content-center w-100">
+			<div className="container border border-dark border-3 rounded col-md-2 p-5 mt-5 ">
+				<div className="d-flex flex-column justify-content-center">
+					<button
+						className={`btn btn-success p-5 rounded-circle mb-3 ${color != "green" && "opacity-25"}`}
+						onClick={() => {
+							if (color == "green") {
+								setColor("")
+								return
+							}
+							setColor("green")
+						}}
+					>
+					</button>
+					<button
+						className={`btn btn-warning p-5 rounded-circle mb-3 ${color != "yellow" && "opacity-25"}`}
+						onClick={() => {
+							if (color == "yellow") {
+								setColor("")
+								return
+							}
+							setColor("yellow")
+						}}
+					>
+					</button>
+					<button
+						className={`btn btn-danger p-5 rounded-circle ${color != "red" && "opacity-25"}`}
+						onClick={() => {
+							if (color == "red") {
+								setColor("")
+								return
+							}
+							setColor("red")
+						}}
+					></button>
+				</div>
+			</div>
+
+
 		</div>
 	);
 };
